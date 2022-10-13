968c863da52e57c676b49fb508ac2b21a6cd86b9
Irnaltherlyei <75485116+Irnaltherlyei@users.noreply.github.com>
2022-10-13 02:04:57 +0200

    Refactoring of frequently used functions into static functions in a dedicated class.

---
+++ lib/date.dart
@@ -0,0 +1,34 @@
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+
+class Date{
+  Date();
+
+  static int daysToBirthday(DateTime birthday){
+    DateTime from = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+    DateTime to = DateTime(DateTime.now().year, birthday.month, birthday.day);
+    return to.difference(from).inDays <= 0 ? DateTime(DateTime.now().year + 1, birthday.month, birthday.day).difference(from).inDays : to.difference(from).inDays;
+  }
+
+  static DateTime now(){
+    return DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day).add(const Duration(hours: 12));
+  }
+
+  static DateTime cutTime(DateTime date){
+    return DateTime(date.year, date.month, date.day);
+  }
+
+  static Future<tz.TZDateTime> convertDateTime(DateTime date) async{
+    tz.initializeTimeZones();
+    tz.setLocalLocation(await getTimeZoneLocation());
+
+    return tz.TZDateTime.from(date, await getTimeZoneLocation());
+  }
+
+  static Future<tz.Location> getTimeZoneLocation() async {
+    String timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+
+    return tz.getLocation(timeZoneName);
+  }
+}
